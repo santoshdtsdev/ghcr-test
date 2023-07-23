0f0c03197f44bdc729e94a524db445cf08cc8639
santoshdtsdev <140302140+santoshdtsdev@users.noreply.github.com>
2023-07-23 12:08:54 +0530

    Added Go files

---
+++ pkg/main.go
@@ -0,0 +1,277 @@
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	"html/template"
+	"net/http"
+	"os"
+	"strconv"
+
+	_ "github.com/lib/pq"
+)
+
+const (
+	DB_USER = "santosh"
+	DB_PASS = "dts123"
+	DB_NAME = "bookstore"
+	// Change to Localhost if testing on local DB build-1
+	// DB_HOST = "localhost"
+	//for build-2 localhost is as below
+	DB_HOST  = "postgres-0.postgres.database.svc.cluster.local"
+	DB_PORT  = 5432
+	SSL_MODE = "disable"
+)
+
+var db *sql.DB
+var tpl *template.Template
+
+type Books struct {
+	Isbn   string
+	Title  string
+	Author string
+	Price  float32
+}
+
+func main() {
+	var err error
+	// db, err = sql.Open("postgres", "postgres://santosh:dts123@postgres.database.svc.cluster.local:5432/bookstore?sslmode=disable")
+	args := fmt.Sprintf("host=%s port=%d dbname=%s user='%s' password=%s sslmode=%s", DB_HOST, DB_PORT, DB_NAME, DB_USER, DB_PASS, SSL_MODE)
+	db, err = sql.Open("postgres", args)
+	if err != nil {
+		fmt.Printf("Connecting to the Database %s", err)
+	}
+	if err = db.Ping(); err != nil {
+		panic(err)
+	}
+	fmt.Println("You are connected to the database...")
+
+	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
+	http.HandleFunc("/", index)
+	http.HandleFunc("/books", booksIndex)
+	http.HandleFunc("/books/show", booksShow)
+	http.HandleFunc("/books/create", booksCreateForm)
+	http.HandleFunc("/books/create/process", booksCreateProcess)
+	http.HandleFunc("/books/update", booksUpdateForm)
+	http.HandleFunc("/books/update/process", booksUpdateProcess)
+	http.HandleFunc("/books/delete/process", booksDeleteProcess)
+	http.Handle("/image/", http.StripPrefix("/image", http.FileServer(http.Dir("./image"))))
+	http.ListenAndServe(":8080", nil)
+}
+
+// func getCommitSHA() (string, error) {
+// 	cmd := exec.Command("git", "rev-parse", "--short", "HEAD")
+// 	output, err := cmd.Output()
+// 	if err != nil {
+// 		return "", err
+// 	}
+// 	// fmt.Printf("OUTPUT: %v", output)
+
+// 	commitSHA := strings.TrimSpace(string(output))
+// 	fmt.Printf("COMMITSHA: %v", commitSHA)
+
+// 	return commitSHA, nil
+// }
+
+func index(w http.ResponseWriter, r *http.Request) {
+	commitSHA := os.Getenv("COMMIT_SHA")
+	fmt.Printf("CommitSHA: %v", commitSHA)
+
+	data := struct {
+		CommitSHA string
+	}{
+		CommitSHA: commitSHA,
+	}
+	tpl.ExecuteTemplate(w, "index.gohtml", data)
+
+	http.Redirect(w, r, "/books", http.StatusSeeOther)
+
+}
+
+func booksIndex(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+		return
+	}
+
+	rows, err := db.Query("SELECT * FROM books")
+	if err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+	defer rows.Close()
+
+	bks := make([]Books, 0)
+	for rows.Next() {
+		bk := Books{}
+		err := rows.Scan(&bk.Isbn, &bk.Title, &bk.Author, &bk.Price)
+		if err != nil {
+			http.Error(w, http.StatusText(500), 500)
+			return
+		}
+		bks = append(bks, bk)
+	}
+	if err = rows.Err(); err != nil {
+		http.Error(w, http.StatusText(500), 500)
+		return
+	}
+
+	tpl.ExecuteTemplate(w, "books.gohtml", bks)
+}
+
+func booksShow(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+		return
+	}
+
+	isbn := r.FormValue("isbn")
+	if isbn == "" {
+		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+		return
+	}
+
+	row := db.QueryRow("SELECT * FROM books WHERE isbn = $1", isbn)
+
+	bk := Books{}
+	err := row.Scan(&bk.Isbn, &bk.Title, &bk.Author, &bk.Price)
+	switch {
+	case err == sql.ErrNoRows:
+		http.NotFound(w, r)
+		return
+	case err != nil:
+		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+		return
+	}
+
+	tpl.ExecuteTemplate(w, "show.gohtml", bk)
+}
+
+func booksCreateForm(w http.ResponseWriter, r *http.Request) {
+	tpl.ExecuteTemplate(w, "create.gohtml", nil)
+}
+
+func booksCreateProcess(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+		return
+	}
+
+	// get form values
+	bk := Books{}
+	bk.Isbn = r.FormValue("isbn")
+	bk.Title = r.FormValue("title")
+	bk.Author = r.FormValue("author")
+	p := r.FormValue("price")
+
+	// validate form values
+	if bk.Isbn == "" || bk.Title == "" || bk.Author == "" || p == "" {
+		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+		return
+	}
+
+	// convert form values
+	f64, err := strconv.ParseFloat(p, 32)
+	if err != nil {
+		http.Error(w, http.StatusText(406)+"Please hit back and enter a number for the price", http.StatusNotAcceptable)
+		return
+	}
+	bk.Price = float32(f64)
+
+	// insert values
+	_, err = db.Exec("INSERT INTO books (isbn, title, author, price) VALUES ($1, $2, $3, $4)", bk.Isbn, bk.Title, bk.Author, bk.Price)
+	if err != nil {
+		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+		return
+	}
+
+	// confirm insertion
+	tpl.ExecuteTemplate(w, "created.gohtml", bk)
+}
+
+func booksUpdateForm(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+		return
+	}
+
+	isbn := r.FormValue("isbn")
+	if isbn == "" {
+		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+		return
+	}
+
+	row := db.QueryRow("SELECT * FROM books WHERE isbn = $1", isbn)
+
+	bk := Books{}
+	err := row.Scan(&bk.Isbn, &bk.Title, &bk.Author, &bk.Price)
+	switch {
+	case err == sql.ErrNoRows:
+		http.NotFound(w, r)
+		return
+	case err != nil:
+		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+		return
+	}
+	tpl.ExecuteTemplate(w, "update.gohtml", bk)
+}
+
+func booksUpdateProcess(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "POST" {
+		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+		return
+	}
+
+	// get form values
+	bk := Books{}
+	bk.Isbn = r.FormValue("isbn")
+	bk.Title = r.FormValue("title")
+	bk.Author = r.FormValue("author")
+	p := r.FormValue("price")
+
+	// validate form values
+	if bk.Isbn == "" || bk.Title == "" || bk.Author == "" || p == "" {
+		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+		return
+	}
+
+	// convert form values
+	f64, err := strconv.ParseFloat(p, 32)
+	if err != nil {
+		http.Error(w, http.StatusText(406)+"Please hit back and enter a number for the price", http.StatusNotAcceptable)
+		return
+	}
+	bk.Price = float32(f64)
+
+	// insert values
+	_, err = db.Exec("UPDATE books SET isbn = $1, title=$2, author=$3, price=$4 WHERE isbn=$1;", bk.Isbn, bk.Title, bk.Author, bk.Price)
+	if err != nil {
+		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+		return
+	}
+
+	// confirm insertion
+	tpl.ExecuteTemplate(w, "updated.gohtml", bk)
+}
+
+func booksDeleteProcess(w http.ResponseWriter, r *http.Request) {
+	if r.Method != "GET" {
+		http.Error(w, http.StatusText(405), http.StatusMethodNotAllowed)
+		return
+	}
+
+	isbn := r.FormValue("isbn")
+	if isbn == "" {
+		http.Error(w, http.StatusText(400), http.StatusBadRequest)
+		return
+	}
+
+	// delete book
+	_, err := db.Exec("DELETE FROM books WHERE isbn=$1;", isbn)
+	if err != nil {
+		http.Error(w, http.StatusText(500), http.StatusInternalServerError)
+		return
+	}
+
+	http.Redirect(w, r, "/books", http.StatusSeeOther)
+}
